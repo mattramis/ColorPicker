55a41b5603e19429276d2c8305a0f4435a48f61e
mattramis <121640749+mattramis@users.noreply.github.com>
2023-07-24 21:00:45 -0500

    origin

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 // Write your Color component here
+import React, { useState } from "react";
 
-const App = () => {
+const Color = ({ color, setSelectedColor }) => {
+  return <div className={color} onClick={() => setSelectedColor(color)}></div>;
+};
+
+const Picker = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+
   return (
-    <div id="container">
-      <div id="navbar">
-        <div>Currently selected: </div>
-        <div className="red">red</div>
+    <div id="navbar">
+      <div>Currently selected: </div>
+      <div className={selectedColor}>{selectedColor}</div>
+
+      <div id="colors-list">
+        <Color color="red" setSelectedColor={setSelectedColor} />
+        <Color color="blue" setSelectedColor={setSelectedColor} />
+        <Color color="green" setSelectedColor={setSelectedColor} />
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
     </div>
   );
 };
 
-export default App;
+export default Picker;
